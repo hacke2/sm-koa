98d5fcc140cff922bb2592f4809357597430095a
hacke2 <hacke2@qq.com>
2014-12-08 14:22:57 +0800

    Signed-off-by: hacke2 <hacke2@qq.com>

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 var koa = require('koa'),
 	router = require('koa-router'),
-	views = require('koa-views'),
+	views = require('co-views'),
+	data = require('./data/index')
+	render = views(__dirname + '/views', {
+	  map: { html: 'ejs' }
+	}),
 	staticServer = require('koa-static'),
 	app = koa(),
 	PORT = 3000;
  
-app.use(views('./views', 'jade')); 
-
-//app.use(staticServer(__dirname + '/res')); 
 app.use(router(app));
+//app.use(staticServer(__dirname + '/res')); 
 app.listen(PORT);
 
-app.get('/', function *(next){
-	this.body = 'hello koa';
+
+
+
+
+
+app.get('/', function *(){
+	console.log(data)
+	this.body = yield render('index', {
+		title : '首页',
+		list : data
+	});
 });
 
---
+++ data/index.js
@@ -0,0 +1,21 @@
+var list = [{
+	title : '标题1',
+		url: 'http://m.sm.cn'
+	},{
+		title : '标题2',
+		url:'http://m.sm.cn'
+	},{
+		title : '标题3',
+		url:'http://m.sm.cn'
+	},{
+		title : '标题4',
+		url: 'http://m.sm.cn'
+	},{
+		title : '标题5',
+		url: 'http://m.sm.cn'
+	},{
+		title : '标题6',
+		url: 'http://m.sm.cn'
+	}];
+
+module.exports = list;
